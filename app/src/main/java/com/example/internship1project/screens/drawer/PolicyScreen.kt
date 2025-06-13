2ce95a388ca05f42eed0f0f06d8f0eafafd68d24
Kaustubh <ksd.tcet@gmail.com>
2025-06-13 21:18:48 +0530

    created drawer unique screens

---
+++ app/src/main/java/com/example/internship1project/screens/drawer/PolicyScreen.kt
@@ -0,0 +1,111 @@
+package com.example.internship1project.screens.drawer
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeContent
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Tune
+import androidx.compose.material.icons.outlined.ReportGmailerrorred
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.internship1project.R
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PolicyScreen(
+    navigateBack: () -> Unit
+) {
+    val systemUiController = rememberSystemUiController()
+    val statusBarColor = Color(0xFF0a3579)
+    val navigationBarColor = Color(0xFFe5f2fb)
+
+    SideEffect {
+        systemUiController.setStatusBarColor(color = statusBarColor)
+        systemUiController.setNavigationBarColor(color = navigationBarColor)
+    }
+
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(
+                        text = (stringResource(id = R.string.policy)),
+                        style = MaterialTheme.typography.headlineLarge,
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.onPrimary,
+                    titleContentColor = MaterialTheme.colorScheme.primary
+                ),
+                navigationIcon = {
+                    IconButton(
+                        onClick = {navigateBack()}
+                    ) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Menu",
+                            tint = Color.Black,
+                        )
+                    }
+                },
+                actions = {
+                    Icon(
+                        imageVector = Icons.Filled.Tune,
+                        contentDescription = "App Logo",
+                        tint = Color.Black,
+                        modifier = Modifier.padding(end = 8.dp)
+                    )
+                }
+            )
+        },
+        contentWindowInsets = WindowInsets.safeContent
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+            , horizontalAlignment = Alignment.CenterHorizontally
+            , verticalArrangement = Arrangement.Center
+        ) {
+
+            Icon(
+                imageVector = Icons.Outlined.ReportGmailerrorred,
+                contentDescription = stringResource(R.string.no_records_found),
+                tint = MaterialTheme.colorScheme.secondary,
+                modifier = Modifier.size(100.dp)
+            )
+
+            Spacer(modifier = Modifier.height(dimensionResource(R.dimen.padding_medium)))
+
+            Text(
+                text = stringResource(R.string.no_records_found)
+            )
+        }
+    }
+}
